7b4249bd7bb47edc8a27ad7279ab54f245a4cb3b
Jackson Ming Hu <huming2207@gmail.com>
2022-06-22 22:52:08 +1000

    main: implement main for testing

--- main/si_main.cpp
+++ main/si_main.cpp
@@ -1,6 +1,11 @@
 #include <cstdio>
+#include "ble_manager.hpp"
+#include "config_manager.hpp"
 
 extern "C" void app_main(void)
 {
+    ESP_ERROR_CHECK(config_manager::instance().init());
+    ESP_ERROR_CHECK(ble_manager::instance().init());
 
+    vTaskDelay(portMAX_DELAY);
 }
