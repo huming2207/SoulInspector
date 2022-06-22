90672f79e147b1232edb7ee49c20ca3e506a836c
Jackson Ming Hu <huming2207@gmail.com>
2022-06-22 22:41:24 +1000

    uart_ctrl: properly set the visibility

--- main/driver/uart_ctrl.hpp
+++ main/driver/uart_ctrl.hpp
@@ -23,16 +23,14 @@ public:
     void operator=(uart_ctrl const &) = delete;
 
 public:
-    esp_err_t init(gpio_num_t _rx, gpio_num_t _tx, gpio_num_t _rts = GPIO_NUM_NC,
+    esp_err_t init(gpio_num_t _rx = GPIO_NUM_1, gpio_num_t _tx = GPIO_NUM_2, gpio_num_t _rts = GPIO_NUM_NC,
                    gpio_num_t _cts = GPIO_NUM_NC, uint32_t _default_baud = 115200, uart_port_t _port = UART_NUM_1);
     esp_err_t deinit();
     [[nodiscard]] bool inited() const;
     static esp_err_t load_uart_config(uart_config_t *cfg);
-
-protected:
+    void set_incoming_data_cb(const std::function<void(size_t)> &cb);
     esp_err_t uart_send(uint8_t *buf, size_t len) const;
     esp_err_t uart_recv(uint8_t *buf, size_t len) const;
-    void set_incoming_data_cb(const std::function<void(size_t)> &cb);
 
 private:
     uart_ctrl() = default;
